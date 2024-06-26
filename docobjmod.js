05d49666c3246bcc4d99bd97ab4eb4b13b4eb4eb
Gowtham-Chitturi <gowthamchitturi3@gmail.com>
2024-06-26 20:53:49 +0530

    kaya

---
+++ docobjmod.js
@@ -0,0 +1,29 @@
+// let button = document.getElementById("myid"); //h1
+// console.dir(button);
+
+// let headings = document.getElementsByClassName("myclass");
+// console.dir(headings);
+// console.log(headings);
+
+// let parahs= document.getElementsByTagName("p");
+// console.dir(parahs)
+
+// let FirstEl = document.querySelector("p");
+// console.dir(FirstEl);
+
+// let allEl = document.querySelectorAll("p");
+// console.dir(allEl);
+
+// //practice
+
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText)
+
+// h2.innerText = h2.innerText + "  From Giet college"; //concatinate
+
+//practice 2
+
+let divs = document.querySelectorAll(".box");
+divs[0].innerText = "new unique value 1";
+divs[1].innerText = "new unique value 2";
+divs[2].innerText = "new unique value 3";
